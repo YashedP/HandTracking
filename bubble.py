680b041188edcc3641283280be6e9800bb9bc262
YashedP <yashjani144@gmail.com>
2023-03-13 01:49:39 -0400

    score, respawn bubble, performance, speed

--- bubble.py
+++ bubble.py
@@ -2,28 +2,35 @@ import math
 
 
 class Bubble:
+    # static variables
     monitorXPixels = 0
     monitorYPixels = 0
-
+    bubblesPopped = 0
+    
     def __init__(self, x, y, imageShape, isPopped=False, distanceFromFinger=100, ):
         self.x = x
         self.y = y
-        self.xPixels = imageShape[0]
-        self.yPixels = imageShape[1]        
+        self.xPixels = imageShape[1]
+        self.yPixels = imageShape[0]
         self.isPopped = isPopped
         self.distanceFromFinger = distanceFromFinger
+        self.distanceThreshold = self.xPixels / 2
     
     def calculateDistance(self, xCoordinateOfFinger, yCoordinateOfFinger):
         self.distanceFromFinger = math.sqrt(pow((self.x + self.xPixels / 2) - xCoordinateOfFinger, 2) + pow((self.y + self.yPixels / 2) - yCoordinateOfFinger, 2))
 
     def popBubble(self):
-        if self.distanceFromFinger <= 65:
+        if self.distanceFromFinger <= self.distanceThreshold:
+            Bubble.bubblesPopped += 1
             self.isPopped = True
 
     def setMonitorDimension(self, monitorXPixels, monitorYPixels):
         Bubble.monitorXPixels = monitorXPixels
         Bubble.monitorYPixels = monitorYPixels
     
-# add function to have another bubble object to check if there's an collisions
-# function for a creating another vector
-# add function for checking which side was there a collision, prerequisite, it must be in the distance below 65 from the center of the both bubbles
+    # Prerequisite, the distance of the 2 bubbles must be less than 100 (2 bubble radiuses)
+    def changeDirection(self, otherBubble):
+        if otherBubble.x > self.x:
+            return "left"
+        else:
+            return "right"
--- festivalProject.py
+++ festivalProject.py
@@ -1,17 +1,35 @@
-# idea, change to gpu usage
 # animations
-# add a bubble object class
-# add a course of the bubble to go, directions
+# bezier's curve
+# bubbles come in from the top and trickle down from the top of the screen going down.
+#! Meaning, I must splice up the images by 100 pictures, ranging from the rows being shown and disappearing
 # Have a frame bigger than what we show to encapture the hands in case we wanted to pop the bubble but it was off screen so it wouldn't be able to find the bubble if half of our hand was off screen
-# have 2 threads, one main thread, one thread does bubble work, the main thread does the hand work, another does the bubble work then waits when both are finished
 
 import cv2
 import mediapipe as mp
 import time
-import cvzone
-import math
+import random
 from bubble import Bubble
+import numpy as np
 
+def imageOverlay(bubbles, image):
+    for i in range(len(bubbles)):
+        if not bubbles[i].isPopped:
+            x = bubbles[i].x
+            y = bubbles[i].y
+
+            roi = image[y:y+imageFront.shape[0], x:x+imageFront.shape[1]]
+
+            img2gray = cv2.cvtColor(imageFront, cv2.COLOR_BGR2GRAY)
+            ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+            mask_inv = cv2.bitwise_not(mask)
+
+            img1_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+            img2_fg = cv2.bitwise_and(imageFront, imageFront, mask=mask)
+            dst = cv2.add(img1_bg, img2_fg)
+
+            image[y:y+imageFront.shape[0], x:x+imageFront.shape[1]] = dst
+    
+    return image
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -19,8 +37,8 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 
 # Change these to change the resolution of the window output
-monitorXPixels = 1280
-monitorYPixels = 720
+monitorXPixels = 1280 # 1280, or 1980
+monitorYPixels = 720 # 720 or 1080
 
 
 
@@ -30,15 +48,17 @@ frames = 0
 
 
 
-imageFront = cv2.imread("Assets/bubble.jpeg", cv2.IMREAD_UNCHANGED)
+# imageFront = cv2.imread("Assets/bubble.jpeg", cv2.IMREAD_UNCHANGED)
+imageFront = cv2.imread("Assets/original.png")
+imageFront = cv2.resize(imageFront, (100, 100))
 
-
-
-bubble1 = Bubble(200, 200, imageFront.shape) 
-bubble2 = Bubble(500, 100, imageFront.shape)
-bubble3 = Bubble(700, 500, imageFront.shape)
-bubble4 = Bubble(1000, 500, imageFront.shape)
-bubbles = [bubble1, bubble2, bubble3, bubble4]
+bubble1 = Bubble(100, 100, imageFront.shape) 
+bubble2 = Bubble(200, 200, imageFront.shape)
+bubble3 = Bubble(600, 300, imageFront.shape)
+bubble4 = Bubble(800, 400, imageFront.shape)
+bubble5 = Bubble(630, 80, imageFront.shape)
+bubble6 = Bubble(700, 120, imageFront.shape)
+bubbles = [bubble1, bubble2, bubble3, bubble4, bubble5, bubble6]
 
 bubble1.setMonitorDimension(monitorXPixels, monitorYPixels)
 
@@ -46,9 +66,8 @@ bubble1.setMonitorDimension(monitorXPixels, monitorYPixels)
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, monitorXPixels)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, monitorYPixels)
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1980)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
+showResults = False
 
 with mp_hands.Hands(
     model_complexity=0,
@@ -56,7 +75,8 @@ with mp_hands.Hands(
     min_tracking_confidence=0.5) as hands:
     while cap.isOpened():
         success, image = cap.read()
-        
+        # monitorXPixels, monitorYPixels = cap.read()
+
         # if camera not Found
         if not success:
             print("Ignoring empty camera frame.")
@@ -71,7 +91,9 @@ with mp_hands.Hands(
         fps = 1/(cTime-pTime)
         pTime = cTime
         # Shows FPS on screen
-        cv2.putText(image, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+        # cv2.putText(image, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+        bubblesPopped = bubbles[0].bubblesPopped
+        cv2.putText(image, str(int(bubblesPopped)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
 
 
@@ -86,18 +108,19 @@ with mp_hands.Hands(
         if frames % 3 == 0:
             results = hands.process(image)
         
-       
+        
         # Example of getting index finger tip landmark coordinates.
         try:
             index_finger_tip = results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
 
             # Accessing x,y,z coordinates of index finger tip landmark
-            x_coord_of_index_finger_tip = (index_finger_tip.x * 1280)
-            y_coord_of_index_finger_tip = (index_finger_tip.y * 720)
+            x_coord_of_index_finger_tip = (index_finger_tip.x * monitorXPixels)
+            y_coord_of_index_finger_tip = (index_finger_tip.y * monitorYPixels)
 
             for i in range(len(bubbles)):
                 bubbles[i].calculateDistance(x_coord_of_index_finger_tip, y_coord_of_index_finger_tip)
-                bubbles[i].popBubble()
+                if not bubbles[i].isPopped:
+                    bubbles[i].popBubble()
         except:
             pass
             
@@ -105,36 +128,42 @@ with mp_hands.Hands(
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         
-        
-        if results.multi_hand_landmarks:
+        if cv2.waitKey(1) & 0xFF == 13:
+            showResults = not showResults
+
+        if results.multi_hand_landmarks and showResults:
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
-                    image,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style())
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
         
-
-
 
 
         # if bubble not "popped"
-        #! Do everything bubble related on a separate thread
         for i in range(len(bubbles)):
             if not bubbles[i].isPopped:
-                image = cvzone.overlayPNG(image, imageFront, [bubbles[i].x, bubbles[i].y])
+                image = imageOverlay(bubbles, image)
 
 
-        
-        
-        
+
+        for i in range(len(bubbles)):
+            if bubbles[i].y > monitorYPixels - bubbles[i].yPixels - 25:
+                max = 1
+            else:
+                max = 8
+            bubbles[i].y += random.randint(1, max)
+            if bubbles[i].y > monitorYPixels - bubbles[i].yPixels:
+                bubbles[i].y = 0
+                bubbles[i].isPopped = False
         
         
         # Flip the image horizontally for a selfie-view display.
         cv2.imshow("MediaPipe Hands", image)
         # cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
         frames += 1
-        if cv2.waitKey(5) & 0xFF == 27:
+        if cv2.waitKey(1) & 0xFF == 27:
             break
 cap.release()
